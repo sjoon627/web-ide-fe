a4cc900eb6bded26af17a53344e5f62fedaed5b1
sjoon627 <sjoon627@naver.com>
2025-08-02 19:19:59 +0900

    fix: LogoSvg 경로 대소문자 수정

--- src/components/ide/TopBar.tsx
+++ src/components/ide/TopBar.tsx
@@ -1,5 +1,5 @@
 import { FigmaIcons, LucideIcons } from "@/assets/icons"
-import LogoSvg from "@/assets/Logo.svg"
+import LogoSvg from "@/assets/logo.svg"
 import { ProjectAvatars } from "@/components/project-list/ProjectAvatars"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Button } from "@/components/ui/custom-button"
